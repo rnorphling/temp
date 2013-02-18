419ad8f7cc8e504535e785699882473e06155c1d
43294686 <43294686@A3441XLZWD4PDWY.HBAP.ADROOT.HSBC>
2013-02-18 15:53:07 +0800

    initial commit

---
+++ HelloGit/src/com/git/HelloGit.java
@@ -0,0 +1,13 @@
+package com.git;
+
+public class HelloGit {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        System.out.println("HellGitGit!!");
+    }
+
+}
